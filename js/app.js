dffeeb21269b205aa3068624d8c50554f68405b1
Ryan Regalado <ryan@design48.net>
2014-05-07 12:31:52 -0700

    redo html output

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 var aBoard1 = [
- ['-','-','-','-','-','-','-'],
- ['-','-','-','-','-','-','-'],
- ['-','-','-','R','R','R','R'],
- ['-','-','-','Y','Y','R','Y'],
- ['-','-','-','Y','R','Y','Y'],
- ['-','-','Y','Y','R','R','R']
- ]
-, elGame = document.querySelector('#game');
-; 
+          ['-','-','-','-','-','-','-']
+        , ['-','-','-','-','-','-','-']
+        , ['-','-','-','R','R','R','R']
+        , ['-','-','-','Y','Y','R','Y']
+        , ['-','-','-','Y','R','Y','Y']
+        , ['-','-','Y','Y','R','R','R']
+    ]
+    , elGame = document.querySelector('#game')
+    , elOutput = document.querySelector('#output')
+    ; 
 
 
 
@@ -17,30 +18,46 @@ var Board = function(options) {
     this.gameBoard = typeof options.gameBoard !== 'undefined' ? options.gameBoard : [];
     this.target = typeof options.target !== 'undefined' ? options.target: null;
 
-    function init() {
+    function _init() {
         _drawBoard.call(this);
     }
 
     function _drawBoard() {
-        var i = 0
-            , len = this.gameBoard.length
-            , ulContainer = document.createElement('ul')
-            , liRow
+        var nCol = 0
+            , nRow = 0
+            , lenRows = this.gameBoard.length
+            , fragContainer = document.createDocumentFragment()
+            , ulRow
+            , liCol
+            , lenCol
             ;
 
-        // iterate through game array
-        for(; i < len; i++) {
-            liRow = document.createElement('li');
-            liRow.innerHTML = this.gameBoard[i];
-            ulContainer.appendChild(liRow);
+        // iterate through game array, starting at rows
+        for(; nRow < lenRows; nRow++) {
+            ulRow = document.createElement('ul');
+
+            row = this.gameBoard[nRow];
+            lenCol = row.length;
+
+            // iterate through row columns
+            for(; nCol < lenCol; nCol++) {
+                liCol = document.createElement('li');
+                liCol.innerText = row[nCol];
+                ulRow.appendChild(liCol);
+            }
+            // reset column
+            nCol = 0; 
+
+            // append row to container
+            fragContainer.appendChild(ulRow); 
         }
 
-        // append to body
-        this.target.appendChild(ulContainer);
+        // append container to body
+        this.target.appendChild(fragContainer);
     }
 
 
-    init.call(this);
+    _init.call(this);
 
     return {
         gameBoard: this.gameBoard
