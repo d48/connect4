27d7997c1248cd3d869750a6d11a84472b84f12c
Ryan Regalado <ryan@design48.net>
2014-05-07 12:51:42 -0700

    add sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,10 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         watch: {
+            sass: {
+                files: ['css/sass/*.{scss,sass}'],
+                tasks: ['sass:dist']
+            },
             scripts: {
                 files: ['./*.html','js/*.js','css/*.css','img/*.*'],
                 tasks: [],
@@ -9,10 +13,22 @@ module.exports = function(grunt) {
                     livereload: true
                 }
             }
+        },
+        
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/sass/styles.scss'
+                },
+                options: {
+                    sourcemap: 'true'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
 
     grunt.registerTask('default',['watch']);
 }
